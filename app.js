885d54677ddd438d0dd5eb8bb9e65852260a7fdf
DESKTOP-3PUVA80\Admin <dat.le@alpaca.vn>
2025-06-25 21:22:53 +0700

    Update template

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
-
+const expressHbs = require('express-handlebars');
+const hbs = expressHbs.create({
+    extname: 'hbs',
+    defaultLayout: '404',
+    layoutsDir:'views'
+})
 const app  = express();
+
+app.engine('hbs', hbs.engine);
+app.set('view engine', 'hbs');
+app.set('views', 'views');
 
 const adminRoutes = require('./routes/admin');
 const shopRountes = require('./routes/shop');
@@ -10,11 +19,11 @@ const shopRountes = require('./routes/shop');
 app.use(bodyParser.urlencoded());
 app.use(express.static(path.join(__dirname, 'public')))
 
-app.use('/admin',adminRoutes);
+app.use('/admin',adminRoutes.router);
 app.use(shopRountes);
 
 app.use((req, res, next)=>{
-    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'))
+    res.status(404).render('404', {title: "Page Not Found"})
 })
 
 
